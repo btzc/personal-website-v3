0b6e62777cfe3cdc2229297f884e33009b300196
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2020-05-09 15:15:06 -0700

    added overlay and button to images

--- src/pages/projects/projects.pages.jsx
+++ src/pages/projects/projects.pages.jsx
@@ -5,6 +5,7 @@ import HALP_CLI from '../../assets/javascript_example.PNG';
 import PERSONAL from '../../assets/personal-website.PNG';
 
 import SectionHeader from '../../components/section-header/section-header.component';
+import Icon from '../../components/icon/icon.component';
 
 import './projects.styles.scss';
 
@@ -13,7 +14,15 @@ const Projects = () => (
     <SectionHeader longLat={`48°30'11.7"N 123°22'33.8"W`} title='Projects' />
     <div className='projects--list'>
       <div className='project'>
-        <img className='project--image' src={CRWN} alt='' />
+        <div className='project--image__container'>
+          <div className='project--image__overlay'>
+            <div className='icon--container'>
+              <Icon />        
+              <Icon />
+            </div>        
+          </div>
+          <img className='project--image' src={CRWN} alt='' />      
+        </div>
         <div className='project--contents'>
           <div className='project--contents__title'>
             <a href='My shit website'>Personal Website</a>
@@ -27,10 +36,22 @@ const Projects = () => (
             <li>Node.js</li>
             <li>Node.js</li>
           </ul>
+          <div className='project--contents__links'>
+            <Icon />
+            <Icon />
+          </div>
         </div>
       </div>
       <div className='project'>
-        <img className='project--image' src={HALP_CLI} alt='' />
+        <div className='project--image__container'>
+          <div className='project--image__overlay'>
+            <div className='icon--container'>
+              <Icon />        
+              <Icon />
+            </div>        
+          </div>
+          <img className='project--image' src={HALP_CLI} alt='' />
+        </div>
         <div className='project--contents'>
           <div className='project--contents__title'>
             <a href='My shit website'>Personal Website</a>
@@ -44,10 +65,22 @@ const Projects = () => (
             <li>Node.js</li>
             <li>Node.js</li>
           </ul>
+          <div className='project--contents__links'>
+            <Icon />
+            <Icon />
+          </div>
         </div>
       </div>
       <div className='project'>
-        <img className='project--image' src={PERSONAL} alt='' />
+        <div className='project--image__container'>
+          <div className='project--image__overlay'>
+            <div className='icon--container'>
+              <Icon />        
+              <Icon />
+            </div>        
+          </div>
+          <img className='project--image' src={PERSONAL} alt='' />
+        </div>
         <div className='project--contents'>
           <div className='project--contents__title'>
             <a href='My shit website'>Personal Website</a>
@@ -61,6 +94,10 @@ const Projects = () => (
             <li>Node.js</li>
             <li>Node.js</li>
           </ul>
+          <div className='project--contents__links'>
+            <Icon />
+            <Icon />
+          </div>
         </div>
       </div>
     </div>
